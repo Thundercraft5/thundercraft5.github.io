72059c1af5aafdf1965eeecfd373af311abc27d3
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2021-10-12 23:52:54 -0400

    .

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
-var licenseNotice = // html 
-$(`<div id="copyright-notice">
+const licenseNotice // html 
+= $(`<div id="copyright-notice">
 	<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
 		<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" />
 	</a>.
@@ -8,4 +8,4 @@ $(`<div id="copyright-notice">
 </div>
 `);
 
-$('main').append(licenseNotice);
+$("main").append(licenseNotice);
