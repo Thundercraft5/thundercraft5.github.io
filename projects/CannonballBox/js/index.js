c8da30feefb03d7c488fc6e3daa31b4fc5af1f6f
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2021-10-12 23:52:54 -0400

    .

--- projects/CannonballBox/js/index.js
+++ projects/CannonballBox/js/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase, require-jsdoc */
-import './utils.js';
+import "./utils.js";
 
 /**
  * drag
@@ -10,39 +10,31 @@ import './utils.js';
  * Update `calculateX()` and `calculateY()` to include drag	
  */
 
-$('.input-wrapper label').each(function() {
+$(".input-wrapper label").each(function() {
 	const $this = $(this);
 
-	$this.next().attr('placeholder', $this.text());
-	$this.parent().attr('title', $this.text());
+	$this.next().attr("placeholder", $this.text());
+	$this.parent().attr("title", $this.text());
 });
 
-const prevInputs = JSON.parse(localStorage.getItem('CannonBallProject-prevInputs'));
-const $grid = $('table.grid');
+const prevInputs = JSON.parse(localStorage.getItem("CannonBallProject-prevInputs"));
+const $grid = $("table.grid");
 
-for (let x = 0; ++x < +$grid.attr('data-x');) {
-	const $row = $('<tr>', {
-		'data-x': x,
-	});
+for (let x = 0; ++x < +$grid.attr("data-x");) {
+	const $row = $("<tr>", { "data-x": x });
 
-	for (let y = 0; ++y < +$grid.attr('data-x');) $row.append($('<td>', { 'data-y': y }));
+	for (let y = 0; ++y < +$grid.attr("data-x");) $row.append($("<td>", { "data-y": y }));
 
 	$grid.append($row);
 }
 
-let intervalId, paused;
-
-const $canvas = $('.markerCanvas');
-const ctx = $canvas[0].getContext('2d');
-
-const canvasHeight = $canvas.height()*3;
-const canvasWidth = $canvas.width()*3;
-
-const boxWidth = $canvas.width();
+let intervalId, paused;const $canvas = $(".markerCanvas");
+const ctx = $canvas[0].getContext("2d");const canvasHeight = $canvas.height()*3;
+const canvasWidth = $canvas.width()*3;const boxWidth = $canvas.width();
 const boxHeight = $canvas.height();
 
-$canvas.attr('width', canvasWidth);
-$canvas.attr('height', canvasHeight);
+$canvas.attr("width", canvasWidth);
+$canvas.attr("height", canvasHeight);
 ctx.fillStyle = "rgba(0, 0, 0, 1)";
 
 const [
@@ -52,7 +44,7 @@ const [
 	$bounciness,
 	$mass,
 	$drag,
-] = '#starting-height-input, #angle-input, #speed-input, #bounciness-input, #mass-input, #drag-input'
+] = "#starting-height-input, #angle-input, #speed-input, #bounciness-input, #mass-input, #drag-input"
 	.split(/\s*,\s*/)
 	.map($.bindParamsLimit(1));
 
@@ -72,10 +64,10 @@ let y_initial, x_initial,
 	velocity_x, velocity_y,
 	global_y, global_x, ms,
 	drag, mass, dragAcceleration,
-	drag_velocity_y, drag_velocity_x; 
+	drag_velocity_y, drag_velocity_x;
 
 function start() {
-	if (paused) 
+	if (paused)
 		paused = !paused;
 	else {
 		y_initial = +$startingHeight.val(); // m
@@ -90,20 +82,18 @@ function start() {
 		velocity_y = speed * Math.sin(angle.toRadians()).round(2); // m/s
 		velocity_x = speed * Math.cos(angle.toRadians()).round(2); // m/s
 
-		$('.parabolicBall, .directionalArrow').css({
-			bottom: (y_initial * 20) + 20,
-			left: (x_initial * 20),
+		$(".parabolicBall, .directionalArrow").css({
+			bottom: y_initial * 20 + 20,
+			left: x_initial * 20,
 		});
 
-		$('.directionalArrow').css({
-			transform: `rotate(-${ angle }deg)`,
-		});
+		$(".directionalArrow").css({ transform: `rotate(-${ angle }deg)` });
 
 		ms = 0; // seconds
 		global_y = y_initial;
 		global_x = 0;
 
-		localStorage.setItem('CannonBallProject-prevInputs', JSON.stringify({
+		localStorage.setItem("CannonBallProject-prevInputs", JSON.stringify({
 			speed,
 			angle,
 			bounciness,
@@ -118,7 +108,7 @@ function start() {
 	function calculateY(y2) {
 		const time = interval/1000;
 
-		global_y = 1/2 * (Math.G + drag_velocity_y) * time**2 + (velocity_y * time) + y2;
+		global_y = 1/2 * (Math.G + drag_velocity_y) * time**2 + velocity_y * time + y2;
 
 		return global_y;
 	}
@@ -126,7 +116,7 @@ function start() {
 	function calculateX(x2) {
 		const time = interval/1000;
 
-		global_x = 1/2 * (drag_velocity_x) * time**2 + (velocity_x * time) + x2;
+		global_x = 1/2 * drag_velocity_x * time**2 + velocity_x * time + x2;
 
 		return global_x;
 	}
@@ -134,14 +124,14 @@ function start() {
 	function calculateXVelocity() {
 		const time = interval/1000;
 
-		return velocity_x + (time*drag_velocity_x);
+		return velocity_x + time*drag_velocity_x;
 	}
 
 	function calculateYVelocity() {
 		const time = interval/1000;
 
-		return velocity_y+(time * (Math.G + drag_velocity_y)); 
-	}	
+		return velocity_y+time * (Math.G + drag_velocity_y);
+	}
 
 	function calculateDrag() {
 		let angle = Math.atan(velocity_y/velocity_x);
@@ -149,11 +139,11 @@ function start() {
 
 		if (velocity_x < 0) angle -= Math.PI;
 
-		dragAcceleration = (-drag * vtot) / mass;
+		dragAcceleration = -drag * vtot / mass;
 		drag_velocity_y = dragAcceleration * Math.sin(angle).round(2); // m/s
 		drag_velocity_x = dragAcceleration * Math.cos(angle).round(2); // m/s
 	}
-	
+
 
 	function frame() {
 		calculateDrag();
@@ -165,45 +155,43 @@ function start() {
 		let x = calculateX(global_x);
 
 		if (y < 0.05) {
-			velocity_y = (-velocity_y)*bounciness;
+			velocity_y = -velocity_y*bounciness;
 			y = 0.1;
 		} else if (y*20 > boxHeight) {
-			velocity_y = (-velocity_y)*bounciness;
+			velocity_y = -velocity_y*bounciness;
 			y = 0.5;
 		}
 
 		if (x*20 > boxWidth) {
-			velocity_x = (-velocity_x)*bounciness;
+			velocity_x = -velocity_x*bounciness;
 			x = boxWidth-0.1;
 		} else if (x < 0.05 && ms/1000 > 1) {
 			x = 0.1;
-			velocity_x = (-velocity_x)*bounciness;
+			velocity_x = -velocity_x*bounciness;
 		}
 
 		ms += interval;
 		const [bottom, left] = [y*20, x*20];
 
-		ctx.fillRect(left*3, canvasHeight-((bottom+20)*3), 4, 4);
-		$('.parabolicBall').css({bottom, left});
-		$('.time').text(`Time elapsed: ${ (ms/1000).round(2) }s`);
-		$('.height').text(`Height: ${ y.round(2) }m`);
-		$('.dist').text(`Distance: ${ x.round(2) }m`);
-		$('.vel-x').text(`X velocity: ${ calculateXVelocity().round(2) }m/s`);
-		$('.vel-y').text(`Y velocity: ${ calculateYVelocity().round(2) }m/s`);
-		$('.drag-x').text(`Drag X: ${ drag_velocity_x.round(2) }m/s`);
-		$('.drag-y').text(`Drag Y: ${ drag_velocity_y.round(2) }m/s`);
+		ctx.fillRect(left*3, canvasHeight-(bottom+20)*3, 4, 4);
+		$(".parabolicBall").css({ bottom, left });
+		$(".time").text(`Time elapsed: ${ (ms/1000).round(2) }s`);
+		$(".height").text(`Height: ${ y.round(2) }m`);
+		$(".dist").text(`Distance: ${ x.round(2) }m`);
+		$(".vel-x").text(`X velocity: ${ calculateXVelocity().round(2) }m/s`);
+		$(".vel-y").text(`Y velocity: ${ calculateYVelocity().round(2) }m/s`);
+		$(".drag-x").text(`Drag X: ${ drag_velocity_x.round(2) }m/s`);
+		$(".drag-y").text(`Drag Y: ${ drag_velocity_y.round(2) }m/s`);
 	}
 }
 
-function reset() { 
-	$('.parabolicBall, .directionalArrow').css({
+function reset() {
+	$(".parabolicBall, .directionalArrow").css({
 		bottom: 0,
 		left: 0,
 	});
 
-	$('.directionalArrow').css({
-		transform: "none",
-	});
+	$(".directionalArrow").css({ transform: "none" });
 
 	clearCanvas();
 	clearInterval(intervalId);
@@ -227,6 +215,6 @@ function pause() {
 	clearInterval(intervalId);
 }
 
-$('#start-button').click(start);
-$('#reset-button').click(reset);
-$('#stop-button').click(pause);
+$("#start-button").click(start);
+$("#reset-button").click(reset);
+$("#stop-button").click(pause);
