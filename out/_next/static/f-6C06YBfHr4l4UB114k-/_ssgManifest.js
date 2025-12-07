afe47be91feac97ac4118f0a1c6ce12b4d5e511c
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2025-12-07 15:25:33 -0500

    Fix page extensions

--- out/_next/static/f-6C06YBfHr4l4UB114k-/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002Fabout","\u002Fbad-request","\u002Fexample","\u002Ffarming","\u002Fphilosophy"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
