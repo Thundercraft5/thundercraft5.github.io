66f0a855fea4b33b5be53183dbf2352184da2a05
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2025-12-07 02:46:48 -0500

    WIP: Begin migration of projects to react/tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import MDXProvider from '../components/MDXProvider'
 import NextConfig from '../next.config.js'
+import '../styles/global.scss'
 import Head from 'next/head'
-import { giantText } from './_app.module.scss'
+import { giantText, mainContent } from './_app.module.scss'
 import Link from 'next/link';
-export default ({ Component, pageProps }: { Component: React.Component<any, any, any>, pageProps: React.PropsWithChildren }) => (
-  <MDXProvider>
-    <Head>
-      <link rel="icon" href="/favicon.ico" type="image/x-icon"  />
-      
-    </Head>
-    <Component {...pageProps} />
-  </MDXProvider>
-)
+import { TopNav } from '../components/TopNav'
+import Footer from '../components/Footer'
+// import $ from 'jquery'
+
+// globalThis.$ = $; // HACK: make jQuery globally available for legacy project scripts 
+
+export default ({ Component, pageProps }: { Component: any, pageProps: React.PropsWithChildren }) => {
+  // Try to read frontmatter exported from the MDX page module or passed in pageProps
+  const fm = (Component && (Component as any).frontmatter) ?? (pageProps && (pageProps as any).frontmatter) ?? undefined
+
+  // Helpful debug: log when frontmatter is missing vs present
+  if (!fm) {
+    console.debug('[_app] frontmatter missing for page', Component?.displayName || Component?.name || 'Unknown')
+  } else {
+    console.debug('[_app] frontmatter for page', fm)
+  }
+
+  return (
+    <MDXProvider>
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" href='/styles/global.scss' />
+        <title>{fm ? fm.title ?? fm : 'Thundercraft5'}</title>
+      </Head>
+      <TopNav />
+      <main className={mainContent}><Component {...pageProps} /></main>
+      <Footer />
+    </MDXProvider >
+  )
+}
