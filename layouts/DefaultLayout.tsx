151403f0b1186438520cfdb98e01ffe2e250f184
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2025-12-07 21:17:20 -0500

    Add meta tags

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -3,6 +3,7 @@ import { TopNav } from "../components/TopNav";
 import { mainContent } from "../pages/_app.module.scss";
 import Footer from "../components/Footer";
 import Head from "next/head";
+import { Router } from "next/router";
 
 export interface Frontmatter {
     title?: string;
@@ -11,14 +12,9 @@ export interface Frontmatter {
     [key: string]: any;
 }
 
-export default function DefaultLayout({ children, title, frontmatter }: { children: React.ReactNode, title: string, frontmatter: Frontmatter }) {
+export default function DefaultLayout({ children, title, frontmatter, router }: { children: React.ReactNode, title: string, frontmatter: Frontmatter, router: Router }) {
     return (<>
-        <Head>
-            <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-            {/* ✅ Next.js automatically injects the compiled CSS here. Do not add <link rel="stylesheet"> manually. */}
-            <title>{title}</title>
-            <meta name="google-site-verification" content="6uW4e_9-NPFLASD-TLV4cjbbNOb9lptKjdJB8ibxsmg" />
-        </Head>
+
         <TopNav />
 
         {/* ✅ 3. Use the styles object */}
