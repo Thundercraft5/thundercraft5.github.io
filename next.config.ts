c66dcb82887efd205658c057fd67abf681cbcbf8
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2025-12-08 04:09:47 -0500

    Remove tsx from pageExtensions

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  pageExtensions: ["mdx", "tsx"],
+  pageExtensions: ["mdx",],
   output: "export",
   sassOptions: {},
   typescript: { ignoreBuildErrors: true },
