3b016308d090ae25b39ee8fc47fa8e3a2ed929b4
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2025-12-13 02:30:55 -0500

    Add second-level nav

--- components/getIcon.tsx
+++ components/getIcon.tsx
@@ -1,13 +1,15 @@
-export const icons = ["JS", "Lua", "Obsidian", "Chrome", "GitHub", "TS"] as const;
+export const icons = ["JS", "Lua", "Obsidian", "Chrome", "GitHub", "TS", "Local"] as const;
 export type Icon = (typeof icons)[number];
 
 
 import { ComponentError } from "../src/util/errors";
-import { GithubIcon, JavaScriptIcon, LuaIcon, ChromeIcon, ObsidianIcon } from "./icons";
+import { GithubIcon, JavaScriptIcon, LuaIcon, ChromeIcon, ObsidianIcon, LocalIcon } from "./icons";
 import { IconProps } from "./icons/Icon";
 import TypescriptIcon from './icons/TypescriptIcon';
 export function getIcon(icon: Icon, props: Partial<IconProps> = {}) {
     switch (icon) {
+        case "Local":
+            return <LocalIcon {...props} />;
         case "JS":
             return <JavaScriptIcon {...props} />;
         case "Lua":
---
+++ components/icons/LocalIcon.tsx
@@ -0,0 +1,5 @@
+import { Icon, type IconProps } from './Icon'
+
+export function LocalIcon(props: IconProps) {
+    return <Icon {...props} alt="Local" src="/icons/window_dev_tools_regular_icon_202646.svg" />;
+}
